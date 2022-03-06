fd0e77016aae138821dc96bb7ab48b1df33870b8
wiktrek <61013382+wiktrek@users.noreply.github.com>
2022-03-06 15:56:21 +0100

    ok

--- SlashCommands/moderation/ban.js
+++ SlashCommands/moderation/ban.js
@@ -1,28 +1,41 @@
-const { Client, CommandInteraction } = require('discord.js')
+const { Client, CommandInteraction } = require('discord.js');
 module.exports = {
-    name: 'ban',
-    description: 'ban a member',
-    userPerm: ["BAN_MEMBERS"],
-    options: [ 
-        {
-            name: 'target',
-            description: 'target to ban',
-            type: 'USER',
-            required: true
+  name: 'ban',
+  description: 'ban a member',
+  userPerm: ['BAN_MEMBERS'],
+  options: [
+    {
+      name: 'target',
+      description: 'target to ban',
+      type: 'USER',
+      required: true,
     },
-    {        name: 'reason',       description: 'reason for this ban',
-        type: 'STRING',
-        required: false
-    }
-],
-    run: async(client, interaction, args) => {
-        const target = interaction.options.getMember('target')
-        const reason = interaction.options.getString('reason'|| "No reason provided")
+    {
+      name: 'reason',
+      description: 'reason for this ban',
+      type: 'STRING',
+      required: false,
+    },
+  ],
+  run: async (client, interaction, args) => {
+    const target = interaction.options.getMember('target');
+    const reason = interaction.options.getString(
+      'reason' || 'No reason provided'
+    );
 
-
-        if(target.roles.highest.position >= interaction.member.roles.highest.position) return interaction.followUp({ content: 'you cant take action on this user as their role is higher than yours'})
-    await target.send(`You have been banned from ${interaction.guild.name}, reason: ${reason}`)
-    target.ban({reason})
-    interaction.followUp({ content: `Banned ${target.user.tag} successfully! reason: ${reason}` })
-    }
-}
+    if (
+      target.roles.highest.position >= interaction.member.roles.highest.position
+    )
+      return interaction.followUp({
+        content:
+          'you cant take action on this user as their role is higher than yours',
+      });
+    await target.send(
+      `You have been banned from ${interaction.guild.name}, reason: ${reason}`
+    );
+    target.ban(reason);
+    interaction.followUp({
+      content: `Banned ${target.user.tag} successfully! reason: ${reason}`,
+    });
+  },
+};
