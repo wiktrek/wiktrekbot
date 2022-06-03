9a07bd523c210020fce88b57bd05c3de9257ac5d
wiktrek <61013382+wiktrek@users.noreply.github.com>
2022-06-03 23:10:08 +0200

    modified MessageCreate event

--- events/message/messageCreate.js
+++ events/message/messageCreate.js
@@ -4,6 +4,12 @@ module.exports = {
 };
 
 client.on('messageCreate', async (message) => {
+  if (message.author.id === '499665258038820866') {
+    if (message.content === 'wiktrekbot test') {
+      message.channel.send('Working!');
+    }
+  }
+  if (message.content === '<@942891118201307156>') message.channel.send('Hi!');
   if (
     message.author.bot ||
     !message.guild ||
