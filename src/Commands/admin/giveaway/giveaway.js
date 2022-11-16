3f6f5ef88e7a786dad51deb3acf10f41c259b793
wiktrek <61013382+wiktrek@users.noreply.github.com>
2022-11-16 21:10:15 +0100

    giveaway command!

---
+++ src/Commands/admin/giveaway/giveaway.js
@@ -0,0 +1,57 @@
+const {
+  ChatInputCommandInteraction,
+  Client,
+  SlashCommandBuilder,
+} = require('discord.js');
+const DB = require('../../../Schemas/GiveawayDB');
+const { endGiveaway } = require('../../../functions/GiveawayFunctions');
+const { PermissionFlagsBits } = require('discord.js');
+
+module.exports = {
+  data: new SlashCommandBuilder()
+    .setName('giveaway')
+    .setDescription('Create or manage a giveaway')
+    .setDefaultMemberPermissions(PermissionFlagsBits.Administrator)
+    .setDMPermission(false)
+    .addSubcommand((options) =>
+      options.setName('create').setDescription('Create a giveaway')
+    )
+    .addSubcommand((subcommand) =>
+      subcommand
+        .setName('menage')
+        .setDescription('menage a giveaway')
+        .addStringOption((option) =>
+          option
+            .setName('toggle')
+            .setDescription('Provide an option to manage')
+            .setRequired(true)
+            .addChoices(
+              { name: 'End', value: 'end' },
+              { name: 'Pause', value: 'pause' },
+              { name: 'Unpause', value: 'unpause' },
+              { name: 'Reroll', value: 'reroll' },
+              { name: 'Delete', value: 'delete' }
+            )
+        )
+        .addStringOption((subcommand) =>
+          subcommand
+            .setName('message_id')
+            .setDescription('Provide the message ID of the giveaway')
+            .setRequired(true)
+        )
+    ),
+  /**
+   * @param {ChatInputCommandInteraction} interaction
+   * @param {Client} client
+   */
+  async execute(interaction, client) {
+    const subcommand = interaction.options.getSubcommand();
+
+    switch (subcommand) {
+      case 'manage':
+        {
+        }
+        break;
+    }
+  },
+};
