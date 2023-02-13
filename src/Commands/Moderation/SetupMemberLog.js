a5d8bd80ab985d60a3f5d7a2273e536316f13272
wiktrek <61013382+wiktrek@users.noreply.github.com>
2023-02-13 17:07:25 +0100

    SetupMemberLog.js

--- src/Commands/Moderation/SetupMemberLog.js
+++ src/Commands/Moderation/SetupMemberLog.js
@@ -17,5 +17,23 @@ module.exports = {
         .setDescription('Select the logging channel for this system')
         .addChannelTypes(ChannelType.GuildText)
         .setRequired(true)
+    )
+    .addRoleOption((option) =>
+      option
+        .setName('member_role')
+        .setDescription('Set the role to be automatically added to new members')
+        .setRequired(false)
+    )
+    .addRoleOption((option) =>
+      option
+        .setName('bot_role')
+        .setDescription('Set the role to be automatically added to new bots.')
+        .setRequired(false)
     ),
+  /**
+   * @param {ChatInputCommandInteraction} interaction
+   */
+  async execut(interaction, client) {
+    const {} = interaction;
+  },
 };
