54f0d671be95ab0ad0fd93bd3d67777673263044
wiktrek <wiktor.janiszewski10@gmail.com>
2023-07-27 20:29:27 +0200

    Eris

--- src/Schemas/MemberLog.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-const MemberLogSchema = new mongoose.Schema({
-  Guild: String,
-  memberRole: String,
-  botRole: String,
-});
-
-module.exports = mongoose.model('MemberLog', MemberLogSchema);
--- src/Schemas/VotedMembers.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose');
-
-const VotedSchema = new mongoose.Schema({
-  messageId: { type: String, require: true },
-  userId: { type: String, require: true },
-});
-
-module.exports = mongoose.model('VotedMembers', VotedSchema);
