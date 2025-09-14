7b76f1f6337ae9c291e6d909abefd34708d9b797
wiktrek <wiktor.janiszewski10@gmail.com>
2025-09-14 23:11:19 +0200

    started project with eris ( nothing works )

--- src/events/Client/ready.js
+++
@@ -1,9 +0,0 @@
-const { loadCommands } = require("../../handlers/commandHandler");
-module.exports = {
-  name: "ready",
-  once: true,
-  execute(client) {
-    loadCommands(client);
-    console.log("Logged in as:", client.user.username);
-  },
-};
